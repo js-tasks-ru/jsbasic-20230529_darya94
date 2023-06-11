181f9a36f35a6ad29f23b42c6a11a5126bbb13b0
Daria Kozlova <kozlovadarya1@mail.ru>
2023-06-11 20:07:17 +0300

    changing the string

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,15 @@
 function camelize(str) {
-  // ваш код...
+  splitedArr = str.split('-');
+  firstElem = splitedArr[0];
+  return firstElem + splitedArr
+    .slice(1)
+    .map((elem) => {
+    let firstLetter = elem[0].toUpperCase();
+    let newElem = firstLetter;
+    for (i = 1; i < elem.length; i++) {
+      newElem += elem[i];
+    };
+    return newElem
+  })
+    .join('')
 }
