6f79a9e178806c70457a1a348077867a36ebeb1c
Daria Kozlova <kozlovadarya1@mail.ru>
2023-06-21 21:13:43 +0300

    text hider

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
-  // ваш код...
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+  
+  btn.addEventListener('click', textHider);
+  function textHider() {
+    text.hidden =!text.hidden;
+  }
+  
 }
