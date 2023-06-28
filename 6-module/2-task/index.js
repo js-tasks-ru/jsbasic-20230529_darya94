90bcdf35ce22d871243b43f107731e321151f70f
Daria Kozlova <kozlovadarya1@mail.ru>
2023-06-28 17:25:58 +0300

    fixed

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -15,7 +15,7 @@ export default class ProductCard {
 <div class="card__body">
   <div class="card__title">${this.product.name}</div>
   <button type="button" class="card__button">
-      <img src="../../assets/images/icons/plus-icon.svg" alt="icon">
+      <img src="/assets/images/icons/plus-icon.svg" alt="icon">
   </button>
 </div>`
 
@@ -26,4 +26,4 @@ this.elem.querySelector( ".card__button" ).onclick = () => {
   } ) );
   }
 }
-}
+  }
